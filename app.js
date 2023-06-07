f3b992e349e32715b73c588cf8352722bece4293
rahul-g5125 <rahul.h.gandla@gmail.com>
2023-06-07 15:09:06 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const process = require('process');
+const env = require('dotenv').config();
+const https = require('https');
+const request = require('request');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+const port = 3000;
+
+const url = "https://api.unsplash.com/search/photos?page=1&client_id="+process.env.ACCESS_KEY+"&query=pride";
+
+app.get('/', (req, res) => {
+    res.render("search");
+    request(url, function (error, res, body) {
+        if (!error && res.statusCode == 200) {
+            const response = JSON.parse(body);
+            const results = response.results[0];
+        }
+    });
+});
+
+app.post("/", (req, res) => {
+    const query = req.body.search;
+    const url = "https://api.unsplash.com/search/photos?page=10&client_id="+process.env.ACCESS_KEY+"&query="+query+"&orientation=portrait";
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            const resp = JSON.parse(body);
+            const results = resp.results;
+            res.render("home", { images: results });
+        } else {
+            console.log(error);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log("Successfully running on port " + port);
+});
