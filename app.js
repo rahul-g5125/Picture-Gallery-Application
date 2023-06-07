433649ac17842ce8fdcf1bde9880d7ec57ff6f58
rahul-g5125 <rahul.h.gandla@gmail.com>
2023-06-07 17:02:25 +0530

    updating the access key

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 const port = 3000;
+const ACCESS_KEY="LWjTfUL4qC0sIKzm_MVxwa6ZYTGTYK3D3rsQw1bAScg"
 
-const url = "https://api.unsplash.com/search/photos?page=1&client_id="+process.env.ACCESS_KEY+"&query=pride";
+const url = "https://api.unsplash.com/search/photos?page=1&client_id="+ACCESS_KEY+"&query=pride";
 
 app.get('/', (req, res) => {
     res.render("search");
@@ -24,7 +25,7 @@ app.get('/', (req, res) => {
 
 app.post("/", (req, res) => {
     const query = req.body.search;
-    const url = "https://api.unsplash.com/search/photos?page=10&client_id="+process.env.ACCESS_KEY+"&query="+query+"&orientation=portrait";
+    const url = "https://api.unsplash.com/search/photos?page=10&client_id="+ACCESS_KEY+"&query="+query+"&orientation=portrait";
     request(url, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             const resp = JSON.parse(body);
